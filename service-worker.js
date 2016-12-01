f19953ff2be1421af26862d5b2a4418f8280bd72
Jagajit Prusty <pprusty@akamai.com>
2016-12-01 08:49:39 +0530

    service worker & snippets added

---
+++ service-worker.js
@@ -0,0 +1,56 @@
+// Register Service Worker
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('./service-worker.js',{scope : '/'}).then(function(registration) {
+    // Registration was successful
+    console.log('ServiceWorker registration successful with scope: ', registration.scope);
+  }).catch(function(err) {
+    // registration failed :(
+    console.log('ServiceWorker registration failed: ', err);
+  });
+}
+
+
+var cacheName = 'V-1';
+var filesToCache = [
+  '/', '/index.html',
+  '/css/all.css', '/css/material.min.css',
+  '/js/all.js', '/js/material.min.js',
+  '/images/service-worker-1.png','/images/service-worker-2.png','/images/service-worker-3.png',
+];
+
+self.addEventListener('install', function(e) {
+  console.log('[ServiceWorker] Installing');
+  e.waitUntil(
+    caches.open(cacheName).then(function(cache) {
+      console.log('[ServiceWorker] Caching app shell');
+      return cache
+              .addAll(filesToCache) //this is atomic in nature i.e. if any of the file fails the entire cache step fails.
+              .then(() => {console.log('[ServiceWorker] App shell Caching  Successful');})
+              .catch(() => {console.log('[ServiceWorker] App shell Caching  Failed');})
+    })
+  );
+});
+
+
+self.addEventListener('activate', function(e) {
+  console.log('[ServiceWorker] Activate');
+  e.waitUntil(
+    caches.keys().then(function(keyList) {
+      return Promise.all(keyList.map(function(key) {
+        if (key !== cacheName) {
+          console.log('[ServiceWorker] Removing old cache', key);
+          return caches.delete(key);
+        }
+      }));
+    })
+  );
+});
+
+// Basic Caching strategy
+self.addEventListener('fetch', function(e) {
+  e.respondWith(
+    caches.match(e.request).then(function(response) {
+      return response || fetch(e.request);
+    })
+  );
+});
---
+++ snippets.js
@@ -0,0 +1,46 @@
+self.addEventListener('fetch', function(event) {
+  event.respondWith(
+    caches.match(event.request).then(function(resp) {
+      return resp || fetch(event.request).then(function(response) {
+        return caches.open('v1').then(function(cache) {
+          cache.put(event.request, response.clone());
+          return response;
+        });
+      });
+    })
+  );
+});
+
+Cache then network strategy
+self.addEventListener('fetch', function(e) {
+  console.log('[Service Worker] Fetch', e.request.url);
+  var dataUrl = 'https://www.backend-app.com/api/';
+  if (e.request.url.indexOf(dataUrl) > -1) {
+    /*
+     * When the request URL contains dataUrl, the app is asking for fresh
+     * weather data. In this case, the service worker always goes to the
+     * network and then caches the response. This is called the "Cache then
+     * network" strategy:
+     * https://jakearchibald.com/2014/offline-cookbook/#cache-then-network
+     */
+    e.respondWith(
+      caches.open(dataCacheName).then(function(cache) {
+        return fetch(e.request).then(function(response){
+          cache.put(e.request.url, response.clone());
+          return response;
+        });
+      })
+    );
+  } else {
+    /*
+     * The app is asking for app shell files. In this scenario the app uses the
+     * "Cache, falling back to the network" offline strategy:
+     * https://jakearchibald.com/2014/offline-cookbook/#cache-falling-back-to-network
+     */
+    e.respondWith(
+      caches.match(e.request).then(function(response) {
+        return response || fetch(e.request);
+      })
+    );
+  }
+});
